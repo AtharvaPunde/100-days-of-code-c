c8c76def467cf9e60ee790d95aeb388d30daee81
Atharva Nitin Punde <Atharva.41860@stu.upes.ac.in>
2026-08-23 19:58:26 +0530

    first commit

---
+++ Day1/q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    printf("Enter the first number: ")
+    scanf("%d", &a)
+    printf("Enter the second number: ")
+    scanf("%d", &a)
+    int sum = a + b
+    printf("The sum of the two numbers is: %d ", sum )
+    return 0;
+}
